fb058fd46745828264c602dba34a649d9ce6575a
ericksong <ericksong@pptv.com>
2013-07-05 09:22:32 +0800

    remove css-modules folders

---
+++ pages/tv/page-tv.js
@@ -0,0 +1,17 @@
+/**
+ * @author  Erick Song
+ * @email   ahschl0322@gmail.com
+ * @info    电视剧频道JS - 建议命名page-*
+ */
+
+define('page-tv', function(require){
+	var $ = require('jquery'),
+        user = require('user'),
+        login = require('login'),
+        cookie = require('cookie');
+
+    return {
+
+    };
+
+});
